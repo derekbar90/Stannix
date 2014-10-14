c483096d269a7a4c930a780f39e3fa71ef746945
Derek Barrera <derekbarrera@gmail.com>
2014-10-14 11:57:01 -0400

    added printMessages, to text the retrieval of messages into an ArrayList, this will be removed later.

--- src/Stannix/Stannix.java
+++ src/Stannix/Stannix.java
@@ -4,10 +4,12 @@
 package Stannix;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
 
 import com.twilio.sdk.TwilioRestException;
+import com.twilio.sdk.resource.instance.Message;
 
 /**
  * @author derekbarrera
@@ -18,13 +20,12 @@ public class Stannix {
 
 	String phoneNumber, messageContents, currentDateStamp;
 	TextService newText, messageList;
+	String messageListSring = "";
 	
 	public Stannix(String phoneNumber, String messageContents) {
 		
 		this.phoneNumber = phoneNumber;
 		this.messageContents = messageContents;
-		
-		sendText();
 		
 	}
 	
@@ -46,18 +47,35 @@ public class Stannix {
 		
 	}
 	
-	public String getInfo(){
+	public ArrayList<Message> getMessages(){
 		
 		messageList = new TextService();
 		return messageList.getMessageList(setTimeStamp());
 	
 	}
 	
-	public String displaySessionId(){
+	public String printMessages(ArrayList<Message> messageList){
+		
+		for(Message message : messageList){
+			
+		 	if(message.getDirection().equals("inbound")){
+				 
+		 		messageListSring += message.getBody() + " " + message.getDateSent() + "\n";
+		 	
+		 	}
+		 	
+			
+		}
+		
+		return messageListSring;
+		
+	}
+	
+	public String gameInitialization(){
 		
 		Random rand = new Random();
+		setTimeStamp();
 		return String.valueOf(rand.nextInt((9999 - 1001) + 1) + 1001);
-		
 		
 	}
 	
