ff685c35c03b6a7eb89bd6f5bcbd783bfaa5c687
Derek Barrera <derekbarrera@gmail.com>
2014-10-14 11:53:54 -0400

    Created of a player class to assist in the creation and management of player objects. Still needs alot of work.

---
+++ src/Stannix/Player.java
@@ -0,0 +1,16 @@
+package Stannix;
+
+public class Player {
+
+	String phoneNumber, input;
+	int points;
+	
+	public Player(String phoneNumber, String input){
+		
+		this.phoneNumber = phoneNumber;
+		this.input = input;
+		points = 0;
+		
+	}
+	
+}
