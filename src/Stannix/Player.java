024c614869ba9f56f5cb54c79469e1e48bf34036
Derek Barrera <derekbarrera@gmail.com>
2014-10-16 16:06:21 -0400

    Added toString method for better representation of player object.

--- src/Stannix/Player.java
+++ src/Stannix/Player.java
@@ -4,12 +4,20 @@ public class Player {
 
 	String phoneNumber, input;
 	int points;
+	boolean judge;
 	
 	public Player(String phoneNumber, String input){
 		
 		this.phoneNumber = phoneNumber;
 		this.input = input;
 		points = 0;
+		judge = false;
+		
+	}
+	
+	public String toString(){
+		
+		return phoneNumber + " : " + input;
 		
 	}
 	
